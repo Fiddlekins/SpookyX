cb3aebd409d9c2fa6439b10c67bb1571acee7da1
Fiddlekins <Fiddlekins@gmail.com>
2015-04-14 19:53:44 +0100

    Create SpookyX.meta.js

---
+++ SpookyX.meta.js
@@ -0,0 +1,34 @@
+// ==UserScript==
+// Displayable Name of your script
+// @name          SpookyX
+ 
+// Brief description
+// @description   Enhances functionality of FoolFuuka boards. Devloped further for more comfortable ghost-posting on the moe archives.
+ 
+// Your name, copyright  
+// @author        Fiddlekins
+ 
+// Version Number
+// @version       24.9
+ 
+// @include       https://*4plebs.org/*
+// @include       http://*4plebs.org/*
+// @include       https://archive.moe/*
+// @include       http://*loveisover.me/*
+// @include       https://*loveisover.me/*
+// @include       http://*imcute.yt/*
+// @include       https://*imcute.yt/*
+// @include       http://boards.foolz.us/*
+// @include       https://boards.foolz.us/*
+// @include       https://*nyafuu.org/*
+// @include       http://*nyafuu.org/*
+// @include       https://*fgts.jp/*
+// @include       http://*fgts.jp/*
+// @include       https://*not4plebs.org/*
+// @include       http://*not4plebs.org/*
+
+// @grant         none
+// @updateURL     https://github.com/Fiddlekins/SpookyX/blob/master/SpookyX.meta.js
+// @downloadURL   https://github.com/Fiddlekins/SpookyX/blob/master/SpookyX.user.js
+// @icon          http://i.imgur.com/LaYyYRl.png
+// ==/UserScript==
