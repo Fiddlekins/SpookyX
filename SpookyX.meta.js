69e710c540eb987ca61ba83de85838ae912ab67e
Fiddlekins <Fiddlekins@gmail.com>
2015-06-05 02:51:35 +0100

    Update to v28.0

--- SpookyX.meta.js
+++ SpookyX.meta.js
@@ -1,16 +1,8 @@
 // ==UserScript==
-// Displayable Name of your script
 // @name          SpookyX
-
-// Brief description
 // @description   Enhances functionality of FoolFuuka boards. Developed further for more comfortable ghost-posting on the moe archives.
-
-// Your name, copyright  
 // @author        Fiddlekins
-
-// Version Number
-// @version       27.6
-
+// @version       28.0
 // @include       https://*4plebs.org/*
 // @include       http://*4plebs.org/*
 // @include       https://archive.moe/*
@@ -26,7 +18,7 @@
 // @include       http://*fgts.jp/*
 // @include       https://*not4plebs.org/*
 // @include       http://*not4plebs.org/*
-
+// @require       https://cdn.rawgit.com/madapaja/jquery.selection/master/src/jquery.selection.js
 // @grant         none
 // @updateURL     https://github.com/Fiddlekins/SpookyX/raw/master/SpookyX.meta.js
 // @downloadURL   https://github.com/Fiddlekins/SpookyX/raw/master/SpookyX.user.js
