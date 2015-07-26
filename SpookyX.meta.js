c2f23e730c19dc5b0e6397815dce0f98353231f7
Fiddlekins <Fiddlekins@gmail.com>
2015-07-26 01:25:46 +0100

    Updated SpookyX.meta.js to v31.0

--- SpookyX.meta.js
+++ SpookyX.meta.js
@@ -2,7 +2,7 @@
 // @name          SpookyX
 // @description   Enhances functionality of FoolFuuka boards. Developed further for more comfortable ghost-posting on the moe archives.
 // @author        Fiddlekins
-// @version       30.2
+// @version       31.0
 // @namespace     https://github.com/Fiddlekins/SpookyX
 // @include       http://archive.4plebs.org/*
 // @include       https://archive.4plebs.org/*
