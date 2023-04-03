f27cbeabef7b3eda722d9043ef6592b2a75d5969
writer <writer@email>
2023-04-03 19:24:09 +0100

    Synchronize @include urls with 4chenz/archives.json
    Also created a small script, fetch-archive-list.sh,
    to easily keep the archive list up-do-date.

---
+++ fetch-archive-list.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+curl 'https://raw.githubusercontent.com/4chenz/archives.json/gh-pages/archives.json' | jq -jr '.[] | (select(.http == true) | "http://", .domain, "\n" ), (select(.https == true) | "https://", .domain, "\n")' | sort --field-separator=: --key=2 | sed -e 's;^;// @include       ;' -e 's;$;/*;'
+
+#Sample output:
+#// @include       http://arch.b4k.co/*
+#// @include       https://arch.b4k.co/*
+#// @include       http://archive.4plebs.org/*
+#// @include       https://archive.4plebs.org/*
+#// @include       http://archived.moe/*
+#// @include       https://archived.moe/*
+#// @include       http://archiveofsins.com/*
+#// @include       https://archiveofsins.com/*
+#// @include       https://archive.palanq.win/*
+#// @include       https://boards.fireden.net/*
+#// @include       http://desuarchive.org/*
+#// @include       https://desuarchive.org/*
+#// @include       https://eientei.xyz/*
+#// @include       https://thebarchive.com/*
+#// @include       http://thebarchive.com/*
+#// @include       https://warosu.org/*
+#// @include       https://www.tokyochronos.net/*
